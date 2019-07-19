22fe09a25b3a8b0c84d9c3e8ea3e9fb9f54a0e0c
Hamza khan <hk@amdaniglobal.com>
2019-07-19 18:30:21 +0500

    auth file

--- app.py
+++ app.py
@@ -8,30 +8,13 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/Feature')
-def baby():
-    return 'feature'
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
 
 
-@app.route('/pricing')
-def profile():
-    return "pricing"
 
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-
-@app.route('/save', methods=['GET','POST'])
-def save():
-    if request.method == 'POST':
-        fullname = request.form['fullname']
-        cnic = request.form['cnic']
-        age = request.form['age']
-        datalist = [fullname,cnic,age]
-        return render_template('index.html' , datalist=datalist)
 
 
 
